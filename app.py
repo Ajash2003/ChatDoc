1aa8c257983218c1cbda25c55fae6a92818889bc
ArijeetJash <50255321+Ajash2003@users.noreply.github.com>
2024-11-20 09:52:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-from fpdf import FPDF2 as FPDF
+from fpdf2 import FPDF2 as FPDF
 
 load_dotenv()
 
